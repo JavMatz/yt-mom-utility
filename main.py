bbce516d61f37321a1c9c2042cd5a89e47c08b24
Javier Martínez <erick97am@gmail.com>
2025-10-03 17:39:03 -0600

    Sepaarate Bridge class

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 from __future__ import annotations
 
-import subprocess
 import sys
-import json
 
-from PySide6.QtCore import QObject, Slot
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine, QmlElement
 from PySide6.QtQuickControls2 import QQuickStyle
-
-QML_IMPORT_NAME = "io.qt.textproperties"
-QML_IMPORT_MAJOR_VERSION = 1
-
-@QmlElement
-class Bridge(QObject):
-    query : str = ""
-    videos : list[dict] = []
-
-    @Slot(str, result=None)
-    def setQuery(self, newQuery):
-        self.query = newQuery
-
-    @Slot()
-    def searchYT(self):
-        videos = subprocess.run(['yt-dlp', f'ytsearch5:{self.query}', '--flat-playlist', '--print', '%(.{uploader,title,duration_string,id})j'], capture_output=True, text=True)
-
-        print(videos.stdout)
-
-        for line in videos.stdout.splitlines():
-            video = json.loads(line)
-            self.videos.append(video)
+from model import SearchModel
+from bridge import Bridge
 
 if __name__ == '__main__':
 
